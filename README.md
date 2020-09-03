c4d883890b8c862e6710cf345ceb95475383e015
AugustoCiuffoletti <augusto@di.unipi.it>
2020-09-03 19:21:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-m5lnrx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-m5lnrx)
---
+++ index.ts
@@ -0,0 +1,21 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>Un esempio di <i>promise<i></h1>`;
+
+// Definisco una funzione sleep che restituisce una Promise
+// che si risolve dopo un certo intervallo
+function sleep(ms) { 
+  return new Promise((resolve,reject) => {
+    setTimeout(resolve, ms);
+  });
+}
+console.log("Inizio");
+sleep(5000).then(() => { 		// la prima Promise si risolve dopo 5s
+  console.log("Fine primo step");
+  return(sleep(2000));   		// il metodo then ne restituisce una nuova da 2s
+}).then(() => {
+  console.log("Fine secondo step"); 	// quando anche questa si risolve concludo
+});
